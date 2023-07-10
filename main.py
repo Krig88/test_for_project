7bd08a798f8951950b6220aa0c774f73bd195dba
egunuraka <egunuraka@tuta.io>
2023-07-10 19:08:06 +0300

    added arguments (argparse)

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import argparse
+
 from for_logging import log_operations
 from src.configurations.first_configuration import configurate
 from src.configurations.game_config import GameConfig as Conf
 
-log_dir = "logs"
-log_operations.log_init()
-
+args_parser = argparse.ArgumentParser(description="Adventures In Cat Dog World")
+args_parser.add_argument('-l', '--log_dir', type=str, default="logs",
+                         help='provide a name for logs directory')  # path?
+args_parser.add_argument('-c', '--config_file', type=str, default="config",
+                         help='provide a name for config file')  # needs config reader? && path?
 
 if __name__ == "__main__":
-    # print(f"steps:{game.steps}")
+    args = args_parser.parse_args()
+    log_operations.log_init(args.log_dir)
+
     for j in range(Conf.num_of_games + 1):
         log_operations.change_logging_file(f'game{j}.log')
         game = configurate()
